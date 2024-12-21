84b9e6e7ed48de5d79e9af829437e0dc6331b8b7
TravisFredrickson <traviskojinteki@gmail.com>
2024-12-20 16:33:00 -0800

    Reorganized files. Got GUI comms to (crudely) work.

--- GUI/uart_async_rxtxtasks/main/uart_async_rxtxtasks_main.c
+++ GUI/uart_async_rxtxtasks/main/uart_async_rxtxtasks_main.c
@@ -2,9 +2,13 @@
  * FILE INFO
  *############################################################*/
 
-/* Author: Travis Fredrickson.
- * Date: 2024-12-19.
- * Description: Does something on an ESP32-C6-DevKitC-1-N8. */
+/* UART asynchronous example, that uses separate RX and TX tasks
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied. */
 
 /*##############################################################
  * INCLUDES
@@ -14,63 +18,34 @@
  * Standard.
  *============================================================*/
 
-#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*==============================================================
  * ESP.
  *============================================================*/
 
-#include "esp_chip_info.h"
-#include "esp_flash.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
 #include "esp_log.h"
-#include "led_strip.h"
 #include "esp_system.h"
-#include "esp_task_wdt.h"
-#include "sdkconfig.h"
+#include "led_strip.h"
 
 /*==============================================================
  * FreeRTOS.
  *============================================================*/
 
 #include "freertos/FreeRTOS.h"
-#include "freertos/queue.h"
 #include "freertos/task.h"
-
-/*==============================================================
- * User.
- *============================================================*/
-
-#include "button.h"
 
 /*##############################################################
  * DEFINES
  *############################################################*/
 
-/*==============================================================
- * General.
- *============================================================*/
-
-#define ONE_SECOND 1000
-#define STACK_DEPTH 2048
+#define TXD_PIN (GPIO_NUM_4)
+#define RXD_PIN (GPIO_NUM_17)
+#define LED_STRIP_GPIO (GPIO_NUM_8)
 #define TAG "main"
-
-/*==============================================================
- * LED strip.
- *============================================================*/
-
-#define LED_STRIP_GPIO 8
-#define LED_STRIP_GREEN_TASK_PRIORITY 4
-#define LED_STRIP_YELLOW_TASK_PRIORITY 5
-#define LED_STRIP_RED_TASK_PRIORITY 6
-
-/*==============================================================
- * Button.
- *============================================================*/
-
-#define BUTTON_GREEN_GPIO 4
-#define BUTTON_YELLOW_GPIO 5
-#define BUTTON_RED_GPIO 6
-#define BUTTON_CHECK_TASK_PRIORITY 10
 
 /*##############################################################
  * TYPEDEFS
@@ -94,53 +69,22 @@ typedef struct
  * CONSTANTS
  *############################################################*/
 
+static const int RX_BUF_SIZE = 1024;
+
 /*##############################################################
  * GLOBAL VARIABLES
  *############################################################*/
-
-/*==============================================================
- * LED strip.
- *============================================================*/
 
 static led_strip_t led_strip;
 static TaskHandle_t led_strip_green_task_handle = NULL;
 static TaskHandle_t led_strip_red_task_handle = NULL;
 static TaskHandle_t led_strip_yellow_task_handle = NULL;
 
-/*==============================================================
- * Button.
- *============================================================*/
-
-static button_event_t button_event;
-static QueueHandle_t button_events;
-static TaskHandle_t button_check_task_handle = NULL;
-
 /*##############################################################
  * FUNCTION PROTOTYPES
  *############################################################*/
 
-/*==============================================================
- * General.
- *============================================================*/
-
-static void print_chip_information(void);
-
-/*==============================================================
- * LED strip.
- *============================================================*/
-
 static void led_strip_configure(void);
-static void led_strip_update(void);
-static void led_strip_green_task(void *pvParameter);
-static void led_strip_yellow_task(void *pvParameter);
-static void led_strip_red_task(void *pvParameter);
-
-/*==============================================================
- * Button.
- *============================================================*/
-
-static void button_configure(void);
-static void button_check_task(void *pvParameter);
 
 /*##############################################################
  * FUNCTIONS
@@ -150,30 +94,89 @@ static void button_check_task(void *pvParameter);
  * General.
  *============================================================*/
 
-static void print_chip_information(void)
+void init(void)
 {
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with:\n", CONFIG_IDF_TARGET);
-    printf("\t- %u CPU core(s).\n", chip_info.cores);
-    printf("\t- %s%s%s.\n",
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "Wi-Fi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "");
-    printf("\t- %s.\n",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? "802.15.4 (Zigbee/Thread)" : "");
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("\t- Silicon revision v%u.%u.\n", major_rev, minor_rev);
-    if (esp_flash_get_size(NULL, &flash_size) != ESP_OK)
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    led_strip_configure();
+}
+
+/*==============================================================
+ * UART.
+ *============================================================*/
+
+int sendData(const char *logName, const char *data)
+{
+    const int len = strlen(data);
+    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
+    ESP_LOGI(logName, "Wrote %d bytes.", txBytes);
+    return txBytes;
+}
+
+static void tx_task(void *arg)
+{
+    static const char *TX_TASK_TAG = "TX_TASK";
+    esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
+    while (1)
     {
-        printf("Get flash size failed.");
-        return;
+        sendData(TX_TASK_TAG, "Hello world.\n");
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
-    printf("\t- %" PRIu32 " MB %s flash.\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-    printf("\t- Minimum free heap size: %" PRIu32 " bytes.\n", esp_get_minimum_free_heap_size());
+}
+
+static void rx_task(void *arg)
+{
+    static const char *RX_TASK_TAG = "RX_TASK";
+    esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+    uint8_t *data = (uint8_t *)malloc(RX_BUF_SIZE + 1);
+    while (1)
+    {
+        const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, pdMS_TO_TICKS(1000));
+        if (rxBytes > 0)
+        {
+            data[rxBytes] = 0;
+            ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'.", rxBytes, data);
+            ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+
+            if (0)
+            {
+                char *message = "Read something: ";
+                const int len = strlen(message);
+                uart_write_bytes(UART_NUM_1, message, len);
+            }
+            
+            switch (*data)
+            {
+            case 0:
+            case 30:
+            case 48:
+                vTaskResume(led_strip_green_task_handle);
+                break;
+            case 1:
+            case 31:
+            case 49:
+                vTaskResume(led_strip_yellow_task_handle);
+                break;
+            case 2:
+            case 32:
+            case 50:
+                vTaskResume(led_strip_red_task_handle);
+                break;
+            }
+        }
+    }
+    free(data);
 }
 
 /*==============================================================
@@ -252,7 +255,7 @@ static void led_strip_green_task(void *pvParameter)
             // vTaskDelay(pdMS_TO_TICKS(ONE_SECOND));
             /* Wait without blocking. */
             TickType_t starting_tick = xTaskGetTickCount();
-            TickType_t ticks_to_wait = pdMS_TO_TICKS(ONE_SECOND);
+            TickType_t ticks_to_wait = pdMS_TO_TICKS(1000);
             while ((xTaskGetTickCount() - starting_tick) < ticks_to_wait)
             {
                 /* Busy wait. */
@@ -280,7 +283,7 @@ static void led_strip_yellow_task(void *pvParameter)
         {
             /* Wait without blocking. */
             TickType_t starting_tick = xTaskGetTickCount();
-            TickType_t ticks_to_wait = pdMS_TO_TICKS(ONE_SECOND);
+            TickType_t ticks_to_wait = pdMS_TO_TICKS(1000);
             while ((xTaskGetTickCount() - starting_tick) < ticks_to_wait)
             {
                 /* Busy wait. */
@@ -308,7 +311,7 @@ static void led_strip_red_task(void *pvParameter)
         {
             /* Wait without blocking. */
             TickType_t starting_tick = xTaskGetTickCount();
-            TickType_t ticks_to_wait = pdMS_TO_TICKS(ONE_SECOND);
+            TickType_t ticks_to_wait = pdMS_TO_TICKS(1000);
             while ((xTaskGetTickCount() - starting_tick) < ticks_to_wait)
             {
                 /* Busy wait. */
@@ -323,90 +326,46 @@ static void led_strip_red_task(void *pvParameter)
     vTaskDelete(NULL);
 }
 
-/*==============================================================
- * Button.
- *============================================================*/
-
-static void button_configure(void)
-{
-    ESP_LOGI(TAG, "button_configure().");
-    button_events = pulled_button_init(
-        (PIN_BIT(BUTTON_GREEN_GPIO) | PIN_BIT(BUTTON_YELLOW_GPIO) | PIN_BIT(BUTTON_RED_GPIO)),
-        GPIO_PULLUP_ONLY);
-}
-
-static void button_check_task(void *pvParameter)
-{
-    for (;;)
-    {
-        if (xQueueReceive(button_events, &button_event, pdMS_TO_TICKS(ONE_SECOND)))
-        {
-            if ((button_event.pin == BUTTON_GREEN_GPIO) && (button_event.event == BUTTON_DOWN))
-            {
-                vTaskResume(led_strip_green_task_handle);
-            }
-            if ((button_event.pin == BUTTON_YELLOW_GPIO) && (button_event.event == BUTTON_DOWN))
-            {
-                vTaskResume(led_strip_yellow_task_handle);
-            }
-            if ((button_event.pin == BUTTON_RED_GPIO) && (button_event.event == BUTTON_DOWN))
-            {
-                vTaskResume(led_strip_red_task_handle);
-            }
-        }
-        vTaskDelay(pdMS_TO_TICKS(ONE_SECOND / 100));
-    }
-    vTaskDelete(NULL);
-}
-
 /*##############################################################
  * MAIN
  *############################################################*/
 
 void app_main(void)
 {
-    /* Initalize and configure. */
-    print_chip_information();
-    led_strip_configure();
-    button_configure();
-
-    /* Create tasks. */
+    init();
     xTaskCreate(
-        &led_strip_green_task,
-        "led_strip_green_task",
-        STACK_DEPTH,
+        rx_task,
+        "uart_rx_task",
+        1024 * 2,
         NULL,
-        LED_STRIP_GREEN_TASK_PRIORITY,
-        &led_strip_green_task_handle);
+        configMAX_PRIORITIES - 1,
+        NULL);
     xTaskCreate(
-        &led_strip_yellow_task,
-        "led_strip_yellow_task",
-        STACK_DEPTH,
+        tx_task,
+        "uart_tx_task",
+        1024 * 2,
         NULL,
-        LED_STRIP_YELLOW_TASK_PRIORITY,
-        &led_strip_yellow_task_handle);
+        configMAX_PRIORITIES - 2,
+        NULL);
     xTaskCreate(
         &led_strip_red_task,
         "led_strip_red_task",
-        STACK_DEPTH,
+        1024 * 2,
         NULL,
-        LED_STRIP_RED_TASK_PRIORITY,
+        configMAX_PRIORITIES - 3,
         &led_strip_red_task_handle);
     xTaskCreate(
-        &button_check_task,
-        "button_check_task",
-        STACK_DEPTH,
+        &led_strip_yellow_task,
+        "led_strip_yellow_task",
+        1024 * 2,
         NULL,
-        BUTTON_CHECK_TASK_PRIORITY,
-        &button_check_task_handle);
-
-    /* Print task information. */
-    ESP_LOGI(TAG, "uxTaskPriorityGet(led_strip_green_task_handle) = %u.",
-             uxTaskPriorityGet(led_strip_green_task_handle));
-    ESP_LOGI(TAG, "uxTaskPriorityGet(led_strip_yellow_task_handle) = %u.",
-             uxTaskPriorityGet(led_strip_yellow_task_handle));
-    ESP_LOGI(TAG, "uxTaskPriorityGet(led_strip_red_task_handle) = %u.",
-             uxTaskPriorityGet(led_strip_red_task_handle));
-    ESP_LOGI(TAG, "uxTaskPriorityGet(button_check_task_handle) = %u.",
-             uxTaskPriorityGet(button_check_task_handle));
+        configMAX_PRIORITIES - 4,
+        &led_strip_yellow_task_handle);
+    xTaskCreate(
+        &led_strip_green_task,
+        "led_strip_green_task",
+        1024 * 2,
+        NULL,
+        configMAX_PRIORITIES - 5,
+        &led_strip_green_task_handle);
 }
